53783df3d456812664b97b211b558ffe9f7cfd3a
Maggie Ku <yk58@rice.edu>
2022-10-29 15:56:56 -0500

    aaaa

--- server/graphql/typeDefs.js
+++ server/graphql/typeDefs.js
@@ -1,55 +1,90 @@
 const { gql } = require('apollo-server');
-
+//Figure out the questionaire/question heirarchy 
+//
 module.exports = gql`
-    type Token {
-        jwt: ID!
+    enum QuestionType {
+        FREE
+        DROPDOWN
+        SLIDER
+    }
+
+    enum Campusness {
+        ONCAMPUS
+        OFFCAMPUS
+    }
+
+    enum ResCollege {
+        BAKER
+        WILLRICE
+        HANSZEN
+        WIESS
+        JONES
+        BROWN
+        LOVETT
+        SIDRICH
+        MARTEL
+        MCMURTRY
+        DUNCAN
+    }
+
+    # 3 survey instances: common, oncampus, offcampus
+    type Survey {
+        surveyPages: [SurveyPage!]!
+    }
+
+    type SurveyPage {
+        category: String!
+        questions: [Question!]!
     }
 
     type Question {
         id: ID!
         questionText: String!
-        questionType: String!
+        questionType: QuestionType!
         options: [String!]
-        category: String!
         attr: String!
     }
 
     type User {
         id: ID!
+        miscInfo: MiscInfo!
+        onCampusInfo: OnCampusInfo!
+        offCampusInfo: OffCampusInfo!
+    }
+
+    type MiscInfo {
         email: String!
         firstName: String!
         lastName: String!
         gender: String!
-        miscInfo: MiscInfo!
         isOnCampus: Boolean!
-        onCampusAnswers: OnCampusAnswers
-        offCampusAnswers: OffCampusAnswers
-        additionalInfo: String
-    }
-
-    type MiscInfo {
         phoneNumber: String!
         year: String!
         major: String
-        college: String
+        college: ResCollege!
+        additionalInfo: String
     }
     
-    type OnCampusAnswers {
+    type OnCampusInfo {
         temperature: String!
         timeSleep: String!
         timeWakeup: String!
     }
 
-    type OffCampusAnswers {
-    
+    type OffCampusInfo {
+        pricePrefs: Number
+    }
 
+    type Token {
+        jwt: ID!
     }
 
     type Query {
-    getUser(id: ID!): User
-    getUsers: [User]
+        getUser(id: ID!): User
+        getUsers: [User]
     }
+
     type Mutation {
-    signup(email: String!, username: String!, password: String!): String!,
-    login(email: String, username: String, password: String!): Token!,
+        signup(email: String!, username: String!, password: String!): String!,
+        login(email: String, username: String, password: String!): Token!,
     }`
