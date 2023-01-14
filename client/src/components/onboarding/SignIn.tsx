842d30455f8e42d6f79fc102110d59cea3a5595d
TopazFist <jem12@rice.edu>
2023-01-14 14:45:15 -0600

    added new onboarding

--- client/src/components/onboarding/SignIn.tsx
+++ client/src/components/onboarding/SignIn.tsx
@@ -1,82 +1,53 @@
 import React from "react"
 import "../../styles/SignIn.css"
-import { GoogleLogin } from '@react-oauth/google';
-
+import { useGoogleLogin } from '@react-oauth/google';
+     
 const SignIn = (props: any) => {
+    const login = useGoogleLogin({
+        onSuccess: tokenResponse => console.log(tokenResponse),
+        });
+        const LoginButton = (props: any) => {
+           return (
+               <button className="banner-login" onClick={props.onClick}>
+                   {props.children}
+               </button>
+           )
+       }
+       const SignUpButton = (props: any) => {
+        return (
+            <button className="banner-signup" onClick={props.onClick}>
+                {props.children}
+            </button>
+        )
+    }
     return (
     <div className="sign-in">
-        <div className="banner">
-            <div className="banner-content">
-                <div className="banner-text">
-                    <strong>bunkmate</strong> 
-                </div>
-                <div className="banner-right">
-                    {/* <div className="banner-login">
-                        Log in
-                        
-                    </div> */}
-                    <GoogleLogin
-                        onSuccess={props.login}
-                        onError={() => {
-                            console.log('Login Failed');
-                        }}
-                        />
-                    {/* <div className="banner-signup">
-                        Sign up
-                    </div> */}
-                </div>
-            </div>
-        </div>
-        <div className="body">
-            <div className="body-header">
-                    <strong>find a roommate. avoid the hassle.</strong> 
-            </div>
-            <div className="body-little-header">
-            Rice’s very own easy-to-use digital solution for your roommate finding issues for both on and off-campus living.
-            </div>
-            <div className="body-buttons">
-                {/* <div className="banner-login">
-                    Log in
-                </div>
-                <div className="banner-signup">
-                        Sign up
-                </div> */}
-                {/* <GoogleLogin
-                        onSuccess={props.login}
-                        onError={() => {
-                            console.log('Login Failed');
-                        }}
-                        /> */}
-            </div>
-        </div>
-        <div>
-        <div className="main-img">
-            </div>
-        <div className="wavy-border">
-        </div>
-        </div>
-       
-        <div className="lower-body">
-            <div className="lower-body-header">
-                <strong>Bunkmate optimizes your roommate finding experience so you can avoid the hassle.</strong> 
-            </div>
-            <div className="auth-block">
-                <div className="auth-img">
-                </div>
-                <div className="auth-header">
-                    <strong>Authentication</strong> 
-                </div>
-                <div className="auth-text">
-                Every profile is approved, so you can feel secure and safe when using the service. 
-                </div>
-            </div>
+         <div className="banner">
+         <div className="banner-content">
+                 <div className="banner-text">
+                     <strong>bunkmate</strong> 
+                 </div>
+         </div>
+         </div>
+         <div className="body">
+             <div className="body-header">
+                     <strong>find a roommate. avoid the hassle.</strong> 
+             </div>
+             <div className="body-little-header">
+             Rice’s very own easy-to-use digital solution for your roommate finding issues for both on and off-campus living.
+             </div>
+             <div className="body-buttons">
+            <SignUpButton className="banner-signup" onClick={() => login()}>
+                sign up
+            </SignUpButton>
+            <LoginButton className="banner-login" onClick={() => login()}>
+                login
+            </LoginButton>
             
-            
-        </div>
+             </div>
+             </div>    
     </div>
-    
     )
 }
 
 export default SignIn
-
